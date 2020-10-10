d9caecdb51897bda71ff53764e74fff8ceeacfb7
unknown <so2.nemesis@gmail.com>
2020-10-10 15:15:53 +0330

    profile progressing

--- server.js
+++ server.js
@@ -1,10 +1,18 @@
 //Dependencies & Requirements
+require('dotenv').config();
 const express=require("express");
 const bodyParser=require("body-parser");
 const _=require("lodash");
 const mongoose=require("mongoose");
 const app=express();
+const otpGenerator = require("otp-generator");
+const crypto = require("crypto");
+const session = require('express-session');
+const passport = require("passport");
+const passportLocalMongoose = require("passport-local-mongoose");
 
+let otp;
+let loggedIn=false;
 //Setting EJS as our view engine that fetches ejs files from views folder
 app.set('view engine', 'ejs');
 
@@ -16,8 +24,32 @@ app.use(bodyParser.urlencoded({extended: true}));
 //Adding public folder as a static source of our project
 app.use(express.static("public"));
 
+app.use(session({
+  secret: "Our little secret.",
+  resave: false,
+  saveUninitialized: false
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+
 //Connecting to the DataBase on port 27017
 mongoose.connect("mongodb://localhost:27017/daneshjooAppDB",{useNewUrlParser:true,useUnifiedTopology:true,useFindAndModify: false,useCreateIndex: true, });
+
+
+function createNewOTP(){
+    // Generate a 6 digit numeric OTP
+    otp      = otpGenerator.generate(6, {alphabets: false, upperCase: false, specialChars: false});
+    const ttl      = 2 * 60 * 1000; //5 Minutes in miliseconds
+    const expires  = Date.now() + ttl; //timestamp to 5 minutes in the future
+    const data     = `${otp}.${expires}`; // phone.otp.expiry_timestamp
+    const hash     = crypto.createHmac("sha256",process.env.KEY).update(data).digest("hex"); // creating SHA256 hash of the data
+    const fullHash = `${hash}.${expires}`; // Hash.expires, format to send to the user
+    // you have to implement the function to send SMS yourself. For demo purpose. let's assume it's called sendSMS
+    console.log(`Your OTP is ${otp}. it will expire in 1 minutes`);
+    return fullHash;
+}
 
 //Sample questions data base schema
 const sampleQSchema=new mongoose.Schema({
@@ -83,16 +115,22 @@ const usersSchema=new mongoose.Schema({
 
 });
 
-
+usersSchema.plugin(passportLocalMongoose);
 //Creating the user document
 const User=mongoose.model("User",usersSchema);
 
 
 
+passport.use(User.createStrategy());
+
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+
 
 //landing page
 app.get("/",function(req,res){
-  res.render("main");
+  res.render("home",{loggedIn:loggedIn});
 });
 
 //start page
@@ -106,27 +144,66 @@ app.post("/start",function(req,res){
 
   const phoneNumber=req.body.submitPhoneNumber;
 
-const newUser=new User({
-  phone:phoneNumber
+  const newUser=new User({
+    phone:phoneNumber
 
-  });
+    });
+
   newUser.save((err, data) => {
       console.log('Analyzing Data...');
       if(data) {
           console.log('Your data has been successfully saved.');
           newUser.save();
-          res.redirect("/start");
+          res.render("verify",{phoneNumber:phoneNumber});
+          createNewOTP();
   }
   else {
-    console.log('Something went wrong while saving data.');
-    res.redirect("/start");
+    console.log('Repetetive user');
+    res.render("verify",{phoneNumber:phoneNumber});
+    createNewOTP();
+
   }
 
   });
   });
 
 
+app.post("/verify",function(req,res){
+  const phoneNumber=req.body.submitPhoneNumber;
+    const verifyCodeUserEntered=req.body.verificationCode;
 
+    if(verifyCodeUserEntered===otp){
+      console.log("Success");
+      loggedIn=true;
+      res.render("signUp");
+
+    }else{
+      console.log("wrong code");
+      res.render("verify",{phoneNumber:phoneNumber});
+      createNewOTP();
+    }
+});
+
+
+app.post("/back",function(req,res){
+  res.render("start");
+});
+app.post("/signUp",function(req,res){
+  User.updateMany({phone:"09102310378"},
+    {firstName:req.body.submitFirstName,
+    lastName:req.body.submitLastName,
+    university:req.body.submitUniName,
+    email:req.body.submitEmail}, function (err, docs) {
+    if (err){
+        console.log(err)
+    }
+    else{
+        console.log("Updated Docs : ", docs);
+    }
+});
+
+  res.render("home",{loggedIn:loggedIn});
+});
 
 //Opening and starting our server on port 3000
 app.listen(3000, function() {
